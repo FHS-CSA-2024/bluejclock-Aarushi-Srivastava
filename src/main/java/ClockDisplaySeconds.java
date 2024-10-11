fd2031b91b4f2e2ab585c65ad1e2e0ad9553ded0
Aarushi-Srivastava <aarushi2008@gmail.com>
2024-10-10 19:47:41 -0700

    Update ClockDisplaySeconds.java
    Compiled

--- src/main/java/ClockDisplaySeconds.java
+++ src/main/java/ClockDisplaySeconds.java
@@ -1,6 +1,45 @@
+package src.main.java;
 
-public class ClockDisplaySeconds
-{
-    //Extend ClockDisplaySeconds to include a seconds field.
-    //Update all methods and parameters to accomodate this change
+public class ClockDisplaySeconds {
+    private NumberDisplay hours;
+    private NumberDisplay minutes;
+    private NumberDisplay seconds;
+
+    // Constructor with no parameters (initializes time to 00:00:00)
+    public ClockDisplaySeconds() {
+        this.hours = new NumberDisplay(24);  
+        this.minutes = new NumberDisplay(60); 
+        this.seconds = new NumberDisplay(60);  
+    }
+
+    // Constructor with specified hour, minute, and second
+    public ClockDisplaySeconds(int hour, int minute, int second) {
+        this.hours = new NumberDisplay(24);
+        this.minutes = new NumberDisplay(60);
+        this.seconds = new NumberDisplay(60);
+        setTime(hour, minute, second); 
+    }
+
+    // Set the time
+    public void setTime(int hour, int minute, int second) {
+        this.hours.setValue(hour);
+        this.minutes.setValue(minute);
+        this.seconds.setValue(second);
+    }
+
+    // Method to return the time as a string in the format HH:MM:SS
+    public String getTime() {
+        return hours.getDisplayValue() + ":" + minutes.getDisplayValue() + ":" + seconds.getDisplayValue();
+    }
+
+    // Method to simulate the clock ticking by one second
+    public void timeTick() {
+        seconds.increments();  
+        if (seconds.getValue() == 0) {  
+            minutes.increments();
+            if (minutes.getValue() == 0) {  
+            hours.increments();
+            }
+        }
+    }
 }
